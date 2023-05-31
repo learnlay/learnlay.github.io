8c4d75c2c1db6c0ecb2c02659cbf55c8cfbb1daa
Shing Ngan <69004592+learnlay@users.noreply.github.com>
2023-05-31 11:54:11 +0800

    setting for deploy to GitHub pages

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -6,7 +6,9 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
+  site: 'https://learnlay.github.io',
+  // base: '/my-repo',
+  // site: 'https://odyssey-theme.littlesticks.dev/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
